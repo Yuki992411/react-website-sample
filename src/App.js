4f682501b743cb7af88a9ab31bb3df587159f4c0
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-05-28 04:10:48 +0900

    [Fix] change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
